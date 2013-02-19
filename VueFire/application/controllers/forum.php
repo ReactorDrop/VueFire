e12d6579c507e2ec29bf0a0ef620c623ee1930c2
Andrew Seymour <talk@andrewseymour.co.uk>
2013-02-19 19:35:11 +0000

    Begun adding in settings, modified the default controller

---
+++ VueFire/application/controllers/forum.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Forum extends CI_Controller
+{
+	public function index()
+	{
+		#$this->load->view('welcome_message');
+	}
+}
+
+/* End of file forum.php */
+/* Location: ./application/controllers/forum.php */
---
+++ VueFire/application/models/settings_model.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Settings_Model extends CI_Model {
+    /**
+     *
+     *
+     */
+    function __construct()
+    {
+        // inherit
+        parent::__construct();
+    }
+
+
+
+}
