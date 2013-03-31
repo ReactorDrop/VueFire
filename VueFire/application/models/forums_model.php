bc1b620bd960842581f76ba7926f34694efc69d8
Andrew Seymour <talk@andrewseymour.co.uk>
2013-03-31 20:35:15 +0100

    Tidied up the forums model and made some improvements

--- VueFire/application/models/forums_model.php
+++ VueFire/application/models/forums_model.php
@@ -1,112 +1,92 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
 
 class forums_Model extends CI_Model
 {
-    private $arrStorage = array();
+  private $arrStorage = array();
 
-    private $arrForums = array();
-    private $arrLatestPosts = array();
-    private $arrStatistics = array();
-    #private $arrSettings = array();
+  private $arrForums = array();
+  private $arrLatestPosts = array();
+  private $arrStatistics = array();
 
-    /**
-     *
-     *
-     */
-    function __construct()
-    {
-        // inherit
-        parent::__construct();
-    }
+  #private $arrSettings = array();
 
-    /**
-     *
-     */
-    public function get_forums_structure($intForumId = null)
-    {
-        // are we selecting all or just for this one forum?
-        $this->arrForums = $this->db->get('forums')->result_array();
-        $this->arrLatestPosts = $this->db->select(array('post_id', 'topic_id', 'forum_id', 'post_title', 'post_subject', 'post_user_id', 'post_time', 'user_name', 'user_id'))
-                                         ->from('posts')
-                                         ->join('users', 'users.user_id = posts.post_user_id')
-                                         ->group_by('forum_id')
-                                         ->order_by('post_time', 'desc')
-                                         #->order_by('post_id', 'desc')
-                                         ->get()
-                                         ->result_array();
-        $this->arrStatistics = $this->db->get_where('global_stats', array('obj_type' => 1))->result_array();
-
-        // define
-        $this->arrStorage['forums'] = array();
-
-        // define our structure
-        if(!empty($this->arrForums))
-        {
-            // reorder based on parent
-          $arrForumsTemp = array();
-
-          foreach($this->arrForums as $arrSection)
-          {
-            $arrForumsTemp[$arrSection['forum_id']] = $arrSection;
-          }
-
-          // reset and clean
-          $this->arrForums = $arrForumsTemp;
-          unset($arrForumsTemp);
-
-          foreach($this->arrForums as $intKey => $arrSection)
-          {
-            if($arrSection['forum_parent'] === null)
-            {
-              $this->arrStorage['forums'][$intKey] = $arrSection;
-              $this->arrStorage['forums'][$intKey]['forums'] = $this->find_children($arrSection['forum_id']);
-            }
-          }
-
-          /*  foreach($this->arrForums as $arrSection)
-            {
-                print_r($arrSection);
-                switch($arrSection['forum_type'])
-                {
-                    case 'category':
-                        $this->arrStorage['forums'][$arrSection['forum_id']] = (isset($this->arrStorage['forums'][$arrSection['forum_id']]) ? array_merge($this->arrStorage['forums'][$arrSection['forum_id']], $arrSection) : $arrSection);
-                        break;
-                    case 'subforum':
-                    case 'link':
-                    case 'forum':
-                        $this->arrStorage['forums'][$arrSection['forum_parent']]['forums'][$arrSection['forum_id']] = $arrSection;
-                        break;
-                }
-                #$this->arrStorage['forums'];
-            }*/
-        }
-
-        echo('<pre>' . print_r($this->arrStorage['forums'], true) . '</pre>');
-        print_r($this->arrForums);
-    }
-
-  private function find_children($parentid)
+  /**
+   *
+   *
+   */
+  function __construct()
   {
-    $arrtempdata = array();
-    foreach($this->arrForums as $arrSection)
-    {
-      if($arrSection['forum_parent'] === $parentid)
-      {
-        $arrtempdata[$arrSection['forum_id']] = $arrSection;
-        unset($this->arrForums[$arrSection['forum_id']]);
+    // inherit
+    parent::__construct();
+  }
 
-        $arrtempdata[$arrSection['forum_id']]['forums'] = $this->find_children($arrSection['forum_id']);
+  /**
+   *
+   */
+  public function get_forums_structure($intForumId = null)
+  {
+    // are we selecting all or just for this one forum?
+    $this->arrForums = $this->db->get('forums')->result_array();
+    $this->arrLatestPosts = $this->db->select(array('post_id', 'topic_id', 'forum_id', 'post_title', 'post_subject', 'post_user_id', 'post_time', 'user_name', 'user_id'))
+      ->from('posts')
+      ->join('users', 'users.user_id = posts.post_user_id')
+      ->group_by('forum_id')
+      ->order_by('post_time', 'desc')
+      #->order_by('post_id', 'desc')
+      ->get()
+      ->result_array();
+    $this->arrStatistics = $this->db->get_where('global_stats', array('obj_type' => 1))->result_array();
+
+    // define
+    $this->arrStorage['forums'] = array();
+
+    // define our structure
+    if (!empty($this->arrForums))
+    {
+      // reorder based on parent
+      $arrForumsTemp = array();
+
+      foreach ($this->arrForums as $arrSection)
+      {
+        $arrForumsTemp[$arrSection['forum_id']] = $arrSection;
+      }
+
+      // reset and clean
+      $this->arrForums = $arrForumsTemp;
+      unset($arrForumsTemp);
+
+      // loop through to build our forum structure
+      foreach ($this->arrForums as $intKey => $arrSection)
+      {
+        if ($arrSection['forum_parent'] === null)
+        {
+          $this->arrStorage['forums'][$intKey] = $arrSection;
+          $this->arrStorage['forums'][$intKey]['forums'] = $this->find_children($arrSection['forum_id']);
+        }
       }
     }
 
-    if(empty($arrtempdata))
+    // cleanup and return
+    unset($this->arrForums);
+    return $this->arrStorage['forums'];
+  }
+
+  private function &find_children($intParentId)
+  {
+    $arrTempData = array();
+    foreach ($this->arrForums as $arrSection)
     {
-      return array();
+      if ($arrSection['forum_parent'] === $intParentId)
+      {
+        $arrTempData[$arrSection['forum_id']] = $arrSection;
+        unset($this->arrForums[$arrSection['forum_id']]);
+
+        $arrTempData[$arrSection['forum_id']]['forums'] = $this->find_children($arrSection['forum_id']);
+      }
     }
-    else
-    {
-      return $arrtempdata;
-    }
+
+    return $arrTempData;
   }
 
 }
