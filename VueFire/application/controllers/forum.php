6f52febe0df62632d5219016f719fbfe11fb0da8
Andrew Seymour <talk@andrewseymour.co.uk>
2013-03-31 21:09:13 +0100

    Made some formatting changes and started to add the forum list template in

--- VueFire/application/controllers/forum.php
+++ VueFire/application/controllers/forum.php
@@ -1,27 +1,28 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Forum extends CI_Controller
 {
-    /**
-     *
-     */
-    public function index()
-	{
-        $this->output->enable_profiler(TRUE); // temporary
-        // load what we need
-        $this->load->model('forums_model');
-        $this->load->model('template_model');
+  /**
+   *
+   */
+  public function index()
+  {
+    $this->output->enable_profiler(true); // temporary
+    // load what we need
+    $this->load->model('forums_model');
+    $this->load->model('template_model');
 
-        // load data
-        $arrData = $this->forums_model->get_forums_structure();
-        echo('<pre>' . print_r($arrData, true) . '</pre>');
-        // load our html
-        $this->data['header'] = $this->template_model->header();
-        $this->data['content'] = $this->parser->parse($this->settings_model->get('default_template') . '/forum_list', array(), array('show' => false, 'disable_includes' => true));
-        $this->data['footer'] = $this->template_model->footer();
+    // load data
+    $arrData['forums_structure'] = $this->forums_model->get_forums_structure();
+    echo('<pre>' . print_r($arrData, true) . '</pre>');
 
-        $this->parser->parse($this->settings_model->get('default_template') . '/shell', $this->data, array('show' => true));
-	}
+    // load our html
+    $this->data['header'] = $this->template_model->header();
+    $this->data['content'] = $this->parser->parse($this->settings_model->get('default_template') . '/forum_list', $arrData, array('show' => false, 'disable_includes' => true));
+    $this->data['footer'] = $this->template_model->footer();
+
+    $this->parser->parse($this->settings_model->get('default_template') . '/shell', $this->data, array('show' => true));
+  }
 }
 
 /* End of file forum.php */
