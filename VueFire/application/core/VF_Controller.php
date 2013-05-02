b664d7311e4c5a7a5ac825da145f93ac0d3ff9f6
Andrew Seymour <commit@andrewseymour.co.uk>
2013-05-02 23:55:23 +0100

    Fixed dashboard error, there could be a better solution!

--- VueFire/application/core/VF_Controller.php
+++ VueFire/application/core/VF_Controller.php
@@ -13,3 +13,18 @@ class VF_Controller extends CI_Controller
     $this->data['template_name'] = $this->template_model->get_template_name();
   }
 }
+
+/**
+ * i dislike the following code, will need to look for a better solution
+ * @todo look for better solution!
+ */
+class VFA_Controller extends CI_Controller
+{
+  function __construct()
+  {
+    parent::__construct();
+
+    $this->output->enable_profiler(true); // temporary
+    $this->load->model(array('admin_model'));
+  }
+}
---
+++ VueFire/application/models/admin_model.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class admin_Model extends CI_Model
+{
+    /**
+     *
+     *
+     */
+    function __construct()
+    {
+        // inherit
+        parent::__construct();
+    }
+
+    /**
+     *
+     */
+}
